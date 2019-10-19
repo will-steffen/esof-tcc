7516b8a8a13885a36be7e575c7501b723c6e5189
will-steffen <steffen.willian@gmail.com>
2019-10-19 20:45:47 -0300

    Update 1 Migration

---
+++ src/SistemaGinastica/SistemaGinastica.DomainModel/Migrations/20191019234308_Update1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SistemaGinastica.DomainModel.Migrations
+{
+    public partial class Update1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_group_class_instrutor_id_instructor",
+                table: "group_class");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_instrutor",
+                table: "instrutor");
+
+            migrationBuilder.RenameTable(
+                name: "instrutor",
+                newName: "instructor");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_instructor",
+                table: "instructor",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_group_class_instructor_id_instructor",
+                table: "group_class",
+                column: "id_instructor",
+                principalTable: "instructor",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_group_class_instructor_id_instructor",
+                table: "group_class");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_instructor",
+                table: "instructor");
+
+            migrationBuilder.RenameTable(
+                name: "instructor",
+                newName: "instrutor");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_instrutor",
+                table: "instrutor",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_group_class_instrutor_id_instructor",
+                table: "group_class",
+                column: "id_instructor",
+                principalTable: "instrutor",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
